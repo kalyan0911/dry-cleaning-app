53aeec57dff760a59ac8c3846361915b95034e2c
kalyan0911 <kg9693524503@gmail.com>
2021-06-11 17:27:18 +0530

    RegisterPage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+//import './App.css';
+import HomeComponent from './component/HomeComponent';
+import SignupComponent from './component/SignupComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React quickly
-        </a>
-      </header>
+    <div>
+        <Router>
+              
+                <div className="container">
+                    <Switch> 
+                          <Route path = "/" exact component = {HomeComponent}></Route>
+                          <Route path = "/signup" exact component = {SignupComponent}></Route>
+                          
+                    </Switch>
+                </div>
+              
+        </Router>
     </div>
   );
 }
